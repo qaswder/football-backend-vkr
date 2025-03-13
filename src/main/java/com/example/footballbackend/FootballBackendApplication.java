63d9dea58e1a1baa2838d95ab51623a404dd57e8
s_daniil <d.serov@i-teco.ru>
2025-03-13 03:19:04 +0300

    init

---
+++ src/main/java/com/example/footballbackend/FootballBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footballbackend/FootballBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
