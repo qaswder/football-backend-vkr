8f701f2ca69703d2a834066996c311bd9c5e31f8
s_daniil <d.serov@i-teco.ru>
2025-04-27 16:27:44 +0300

    Файлы конфигурации

--- src/main/java/com/example/footballbackend/config/WebConfig.java
+++ src/main/java/com/example/footballbackend/config/WebConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173"); // Убедитесь, что адрес фронта правильный
+        registry
+                .addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:5173");
     }
 }
